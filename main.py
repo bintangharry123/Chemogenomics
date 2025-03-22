8495750527d81883243775d311b59e691b294c3a
bintangharry123 <bintang.harrypramana14@gmail.com>
2025-03-22 15:24:01 +0700

    commit changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from google import genai  
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
